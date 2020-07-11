cf2052b9510b09590239d301737c70dd8ad48bf6
TYTY <tylerlibstag@gmail.com>
2020-07-10 20:25:37 -0400

    added writeFileSync functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,94 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateREADME = require("./utils/generateMarkdown");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const questions = [
+    {   
+        message: "please provide the project name?",
+        name: "title",
+        type: "input"
+    },
+    {   
+        message: "Please provide a table on content",
+        name: "table of content",
+        type: "input"
+    },
+    {   
+        message: "What is the user's GitHub name?",
+        name: "userName",
+        type:"input"
+    },
+    {  
+        message: "Provide a description of the project",
+        name: "description",
+        type:"input"
+    },
+    {   
+        message: "What is the installation process?",
+        name: "installation",
+        type: "input"
+    },
+    {   
+        message: "How will this project be used?",
+        name: "usage",
+        type: "input"
+    },
+    {   
+        message: "What licenses are required with this project?",
+        name: "licenses",
+        type: "input"
+    },
+    {   
+        message: "Who were the contributors to this project?",
+        name: "contribution",
+        type: "input"
+    },
+    {   
+        message: "What is the test process for this project?",
+        name: "test",
+        type: "input"
+    },
+    {   
+        message: "What is the user github email address?",
+        name: "GitHub user email",
+        type: "input"
+    },
+    {   
+        message: "Please provide a profile picture",
+        name: "GitHub profile picture",
+        type: "input"
+    }
 
 ];
 
-function writeToFile(fileName, data) {
-}
+
 
 function init() {
+
+    inquirer.prompt(questions)
+        .then((inquirerResponse) => {
+            console.log("Making ReadMe");
+            promptUser(inquirerResponse)
+          //fs.writeFileSync("ReadMe.md", inquirerResponse,"utf-8");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 
 }
 
 init();
+function promptUser(answers){
+    const readME = generateREADME(answers);
+    fs.writeFileSync("ReadMe.md", readME,"utf-8");
+
+//        return writeFileAsync("READEME.MD", readME);
+}
+    // .then(function (answers) {
+    //     const readME = generateREADME(answers);
+
+    //     return writeFileAsync("READEME.MD", readME);
+    // })
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,42 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
+# Table of Content
+-[description](#description)
+-[installation](#installation)
+-[usage](#usage)
+-[licenses](#licenses)
+-[contribution](#contribution)
+-[test](#test)
+-[username](#username)
+-[profile](#profile)
 
+${data.username}
+##username:
+
+${data.description}
+##description:
+
+${data.installation}
+##installation:
+
+${data.usage}
+##usage:
+
+${data.licenses}
+##licenses:
+
+${data.contribution}
+##contribution:
+
+${data.test}
+##test:
+
+${data.email}
+##email:
+
+${response.profile}
+##profile:
 `;
 }
 
